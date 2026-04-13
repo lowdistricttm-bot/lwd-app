c46c677d851f0a2cc01d4e9285306499efa49d54
[dyad] <lowdistrict.tm@gmail.com>
2026-04-13 10:22:20 +0200

    [dyad] Fornito script SQL per sbloccare utenti esistenti e automatizzare conferme future - wrote 1 file(s)

--- supabase_setup_autoconfirm.sql
+++ supabase_setup_autoconfirm.sql
@@ -1,4 +1,10 @@
--- Funzione per confermare l'email all'istante
+-- 1. Sblocca tutti gli utenti che sono rimasti in attesa di conferma (Eseguire nel SQL Editor di Supabase)
+UPDATE auth.users 
+SET email_confirmed_at = NOW(), 
+    confirmed_at = NOW() 
+WHERE email_confirmed_at IS NULL;
+
+-- 2. Crea la funzione per confermare automaticamente i prossimi utenti
 CREATE OR REPLACE FUNCTION public.auto_confirm_user()
 RETURNS TRIGGER AS $$
 BEGIN
@@ -8,7 +14,7 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql SECURITY DEFINER;
 
--- Trigger che scatta PRIMA dell'inserimento in auth.users
+-- 3. Attiva il trigger per il futuro
 DROP TRIGGER IF EXISTS on_auth_user_created_confirm ON auth.users;
 CREATE TRIGGER on_auth_user_created_confirm
   BEFORE INSERT ON auth.users
