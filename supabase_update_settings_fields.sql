ada2bffbbc67c867323ff08e4690204a6e96281f
[dyad] <lowdistrict.tm@gmail.com>
2026-04-14 20:33:38 +0200

    [dyad] Risoluzione errore di salvataggio impostazioni tramite script SQL per aggiungere le colonne notifiche. - wrote 1 file(s)

--- supabase_update_settings_fields.sql
+++ supabase_update_settings_fields.sql
@@ -1,4 +1,7 @@
--- Aggiunta colonne per salvare le preferenze delle notifiche
+-- Aggiunta colonne per le preferenze delle notifiche alla tabella profiles
 ALTER TABLE public.profiles 
 ADD COLUMN IF NOT EXISTS email_notifications BOOLEAN DEFAULT false,
 ADD COLUMN IF NOT EXISTS push_notifications BOOLEAN DEFAULT true;
+
+-- Assicuriamoci che i permessi RLS siano attivi (dovrebbero già esserlo)
+-- Gli utenti possono aggiornare solo il proprio profilo
