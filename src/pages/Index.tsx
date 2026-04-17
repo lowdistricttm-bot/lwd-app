213c667f4934eccf32ace2df1719d5445b206cc9
[dyad] <lowdistrict.tm@gmail.com>
2026-04-17 17:35:40 +0200

    [dyad] Compattamento spazio tra Shop e Tab in Home - wrote 2 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -51,8 +51,8 @@ const Index = () => {
         
         <FeaturedProducts />
 
-        {/* Sezione Navigazione Rapida - Visibile a TUTTI sotto lo Shop */}
-        <section className="py-24 px-6 bg-black">
+        {/* Sezione Navigazione Rapida - Spazio ridotto rispetto allo Shop */}
+        <section className="pb-24 pt-4 px-6 bg-black">
           <div className="max-w-7xl mx-auto">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 md:gap-8">
               {navigationTabs.map((item, i) => (
