f76e001d3fd286c5ccbaaf4b7fb82f1445f1ca9f
[dyad] <lowdistrict.tm@gmail.com>
2026-04-20 13:24:27 +0200

    [dyad] Resolving React Router v7 warning - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -80,7 +80,7 @@ const App = () => (
           <TooltipProvider>
             <Toaster />
             <Sonner />
-            <BrowserRouter>
+            <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
               <ScrollToTop />
               <AppContent />
             </BrowserRouter>
