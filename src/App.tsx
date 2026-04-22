7f11e4d5e01d9f6dc00e963b8363675d61926abe
[dyad] <lowdistrict.tm@gmail.com>
2026-04-22 13:30:22 +0200

    [dyad] Removed the LowLab feature entirely as requested. - wrote 1 file(s), deleted 2 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -33,7 +33,6 @@ import Chat from "./pages/Chat";
 import PostDetail from "./pages/PostDetail";
 import Marketplace from "./pages/Marketplace";
 import FitmentTool from "./pages/FitmentTool";
-import LowLab from "./pages/LowLab";
 import Leaderboards from "./pages/Leaderboards";
 import NotFound from "./pages/NotFound";
 
@@ -72,7 +71,6 @@ const AppContent = () => {
             <Route path="/chat/:userId" element={<Chat />} />
             <Route path="/marketplace" element={<Marketplace />} />
             <Route path="/fitment" element={<FitmentTool />} />
-            <Route path="/lowlab" element={<LowLab />} />
             <Route path="/leaderboards" element={<Leaderboards />} />
             <Route path="/admin" element={<AdminDashboard />} />
             <Route path="/admin/applications" element={<AdminApplications />} />
