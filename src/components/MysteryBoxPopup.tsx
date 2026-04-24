78b72ef674f2a9630d59de1fcacfb5eddb733f68
[dyad] <lowdistrict.tm@gmail.com>
2026-04-24 14:10:18 +0200

    Updated src/components/MysteryBoxPopup.tsx

--- src/components/MysteryBoxPopup.tsx
+++ src/components/MysteryBoxPopup.tsx
@@ -112,7 +112,7 @@ const MysteryBoxPopup = () => {
                 </div>
                 <div className="bg-white/5 p-4 rounded-2xl border border-white/5 flex flex-col items-center gap-2 text-center">
                   <Ticket size={20} className="text-yellow-500" />
-                  <span className="text-[8px] font-black uppercase tracking-widest text-yellow-500 whitespace-nowrap">Golden Ticket</span>
+                  <span className="text-[6px] font-black uppercase tracking-widest text-yellow-500 whitespace-nowrap">Golden Ticket</span>
                 </div>
               </div>
 
